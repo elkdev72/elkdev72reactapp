db9b90db151f3ba1794b4c41a95e4dd58b4ec044
elkdev72 <elijahkanyia.developer@gmail.com>
2025-05-17 21:56:10 +0300

    my first react component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, React Beginner!</h1>
+      <Welcome />
     </div>
   );
+}
+
+// Create your first component here
+function Welcome() {
+  return <p>Welcome to your first React component!</p>;
 }
 
 export default App;
